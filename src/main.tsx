b76b083a718a381c8997d3936762f54381f5b435
ValeraKaktotak <valerakaktotak@gmail.com>
2023-10-07 13:30:08 +0300

    configured eslint and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.tsx'
+
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById('root')
+
+if (container !== null && container !== undefined) {
+  ReactDOM.createRoot(container).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
