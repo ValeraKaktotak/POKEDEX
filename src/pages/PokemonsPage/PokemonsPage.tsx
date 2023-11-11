d5dcc537f88e9d8aa15bfcb47c8482b3e8050e2b
Valera <valerakaktotak@gmail.com>
2023-11-11 21:17:12 +0200

    start styling pokemons page

--- src/pages/PokemonsPage/PokemonsPage.tsx
+++ src/pages/PokemonsPage/PokemonsPage.tsx
@@ -3,7 +3,7 @@ import { type FC } from 'react'
 import { useRequestPokemonsQuery } from '../../utils/api/hooks/pokemons'
 
 export const PokemonsPage: FC = () => {
-  // limit of pokemons for one request
+  // pokemons limit for each request
   const { data, isLoading, fetchNextPage } = useRequestPokemonsQuery(5)
 
   if (isLoading) {
@@ -11,7 +11,7 @@ export const PokemonsPage: FC = () => {
   }
   return (
     <>
-      <div>
+      <div className='grid grid-cols-3 gap-3 p-5 '>
         {data?.pages.map((pokemonPage) => {
           return pokemonPage.results.map((pokemon) => (
             <div key={pokemon.name}>{pokemon.name}</div>
