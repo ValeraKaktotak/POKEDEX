379de6296d1e9b086bfd1d19a2946bda3943bcf6
Valera <valerakaktotak@gmail.com>
2023-11-22 07:07:12 +0200

    added accessibility to list elem with onClick function and added classNames lib

--- src/pages/PokedexPage/PokedexPage.tsx
+++ src/pages/PokedexPage/PokedexPage.tsx
@@ -1,11 +1,13 @@
-import type { FC } from 'react'
+import { useState, type FC } from 'react'
 
+import classNames from 'classnames'
 import type { IPokemonPage } from '../../utils/api/hooks/pokemon/types'
 import { useRequestPokemonsQuery } from '../../utils/api/hooks/pokemons'
 import styles from './PokedexPage.module.css'
 
 const PokedexPage: FC = () => {
-  const results = useRequestPokemonsQuery(9)
+  const results = useRequestPokemonsQuery(6)
+  const [selectedPokemonId, setSelectedPokemonId] = useState<number>(1)
   const isLoading = results.some((elem) => elem.isLoading)
   const pokemons = results.map((elem) => elem.data as IPokemonPage)
 
@@ -14,13 +16,32 @@ const PokedexPage: FC = () => {
   }
 
   return (
-    <div className={styles.page_container}>
-      <div className={styles.content_container}>
-        <div>card</div>
-        <ul className={styles.list_container}>
+    <div className={styles.page}>
+      <div className={styles.content}>
+        <div className={styles.card}>
+          <div>title</div>
+          <div>img</div>
+          <div>stats</div>
+        </div>
+        <ul className={styles.list}>
           {pokemons.map((elem) => {
+            const isActive = selectedPokemonId === elem.id
             return (
-              <li key={elem.id} className={styles.pokemon_item}>
+              <li
+                key={elem.id}
+                role='option'
+                aria-selected={isActive}
+                tabIndex={0}
+                className={classNames(styles.pokemon_item, {
+                  [styles.active_pokemon_item]: isActive
+                })}
+                onKeyUp={(event) => {
+                  if (event.key === 'Enter') setSelectedPokemonId(elem.id)
+                }}
+                onClick={() => {
+                  setSelectedPokemonId(elem.id)
+                }}
+              >
                 <img
                   className={styles.pokemon_item_image}
                   src={elem.sprites.front_default ?? ''}
