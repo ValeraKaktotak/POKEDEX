f38153e61a935d27597dad20ad22f88af4de459c
Valera <valerakaktotak@gmail.com>
2023-11-26 14:07:55 +0200

    finished with pokedex card

--- src/pages/PokedexPage/PokedexPage.tsx
+++ src/pages/PokedexPage/PokedexPage.tsx
@@ -33,18 +33,43 @@ const PokedexPage: FC = () => {
               #00{selectedPokemon?.id}
             </div>
           </div>
+          <div className={styles.pokemon_card_types}>
+            {selectedPokemon?.types.map((elem) => (
+              <div className={styles.pokemon_card_type} key={elem.type.name}>
+                {elem.type.name}
+              </div>
+            ))}
+          </div>
           <div className={styles.pokemon_card_title_image}>
             <img
               src={selectedPokemon?.sprites.front_default ?? ''}
               alt='pokemon_image'
             />
           </div>
-          <div>
-            {selectedPokemon?.stats.map((elem) => (
-              <div key={elem.stat.name}>
-                {elem.stat.name}:{elem.base_stat}
-              </div>
-            ))}
+          <div className={styles.pokemon_card_info}>
+            <div>
+              <div className={styles.pokemon_card_info_title}>Stats</div>
+              <ul>
+                {selectedPokemon?.stats.map((elem) => (
+                  <li
+                    className={styles.pokemon_card_info_stat}
+                    key={elem.stat.name}
+                  >
+                    {elem.stat.name}: {elem.base_stat}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <div className={styles.pokemon_card_info_title}>Abilities</div>
+              <ul>
+                {selectedPokemon?.abilities.map((elem, index) => (
+                  <li className={styles.pokemon_card_info_stat} key={index}>
+                    {elem.ability.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
         <ul className={styles.list}>
