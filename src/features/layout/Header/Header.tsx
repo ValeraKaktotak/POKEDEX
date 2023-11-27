43f089ced5e40718e417c4079372ba90dab20262
Valera <valerakaktotak@gmail.com>
2023-11-27 17:28:16 +0200

    added simple Layout component with Header component

---
+++ src/features/layout/Header/Header.tsx
@@ -0,0 +1,20 @@
+import type { FC } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../../assets/pokemon_logo.png'
+import styles from './Header.module.css'
+
+export const Header: FC = () => {
+  return (
+    <div className={styles.header_container}>
+      <div className={styles.logo}>
+        <Link to='/'>
+          <img src={logo} alt='Logo' />
+        </Link>
+      </div>
+      <div className={styles.header_menu}>
+        <Link to='/'>Pokemons Page</Link>
+        <Link to='/pokedex'>Pokedex Page</Link>
+      </div>
+    </div>
+  )
+}
---
+++ src/features/layout/index.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react'
+import { Outlet } from 'react-router-dom'
+import { Header } from './Header/Header'
+
+export const Layout: FC = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  )
+}
