6d48fefcc0527c79e83bdf4b7513f4527a65e7aa
Valera <valerakaktotak@gmail.com>
2023-12-04 20:24:33 +0200

    small rename data

--- src/pages/PokemonsPage/PokemonInfo/PokemonInfo.tsx
+++ src/pages/PokemonsPage/PokemonInfo/PokemonInfo.tsx
@@ -8,34 +8,34 @@ interface IPokemonInfo {
 }
 
 export const PokemonInfo: FC<IPokemonInfo> = ({ id }) => {
-  const { data, isLoading } = useRequestPokemonQuery(id)
+  const { data: pokemon, isLoading } = useRequestPokemonQuery(id)
   const pokemonNumber = getPokemonId(id)
-  if (isLoading || !data) {
+  if (isLoading || !pokemon) {
     return null
   }
-  console.log(data)
+  console.log(pokemon)
 
   return (
     <div className={styles.pokemon_card}>
       <div className={styles.pokemon_card_title}>
-        <div className={styles.pokemon_card_title_name}>{data.name}</div>
+        <div className={styles.pokemon_card_title_name}>{pokemon.name}</div>
         <div className={styles.pokemon_card_title_id}>{pokemonNumber}</div>
       </div>
       <div className={styles.pokemon_card_types}>
-        {data.types.map((elem) => (
+        {pokemon.types.map((elem) => (
           <div className={styles.pokemon_card_type} key={elem.type.name}>
             {elem.type.name}
           </div>
         ))}
       </div>
       <div className={styles.pokemon_card_title_image}>
-        <img src={data.sprites.front_default ?? ''} alt='pokemon_image' />
+        <img src={pokemon.sprites.front_default ?? ''} alt='pokemon_image' />
       </div>
       <div className={styles.pokemon_card_info}>
         <div>
           <div className={styles.pokemon_card_info_title}>Stats</div>
           <ul>
-            {data.stats.map((elem) => (
+            {pokemon.stats.map((elem) => (
               <li
                 className={styles.pokemon_card_info_stat}
                 key={elem.stat.name}
@@ -48,7 +48,7 @@ export const PokemonInfo: FC<IPokemonInfo> = ({ id }) => {
         <div>
           <div className={styles.pokemon_card_info_title}>Abilities</div>
           <ul>
-            {data.abilities.map((elem, index) => (
+            {pokemon.abilities.map((elem, index) => (
               <li className={styles.pokemon_card_info_stat} key={index}>
                 {elem.ability.name}
               </li>
