9461fbe6832081cc49a114cf2244bf82646f2595
Valera <valerakaktotak@gmail.com>
2023-12-18 17:18:13 +0200

    create pokemonSpecies request

---
+++ src/utils/api/requests/pokemon-species/id/index.ts
@@ -0,0 +1,26 @@
+import type { AxiosRequestConfig } from 'axios'
+
+import { pokemonAPI } from '../../../instance'
+import type { IPokemonSpecies } from './types'
+
+interface IParams {
+  id: number
+}
+
+interface RequestPokemonSpeciesParams {
+  config?: AxiosRequestConfig
+  params: IParams
+}
+
+export const requestPokemonSpecies = async ({
+  config,
+  params
+}: RequestPokemonSpeciesParams): Promise<IPokemonSpecies> => {
+  const response = await pokemonAPI.get<IPokemonSpecies>(
+    `species/${params.id}`,
+    {
+      ...config
+    }
+  )
+  return response.data
+}
