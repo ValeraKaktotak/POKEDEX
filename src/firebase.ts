a0f473634bfdf2f3a524481530d4d36faa7ef5b0
Valera <valerakaktotak@gmail.com>
2024-01-28 19:35:08 +0200

    finished main FB_config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { getAnalytics } from 'firebase/analytics'
 import { initializeApp } from 'firebase/app'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword
+} from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import type { UserCredential } from '../node_modules/@firebase/auth/dist/auth-public.d'
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyAjng_H_r294YuDhpXd8i0YISP1wx87kTY',
   authDomain: 'pokedex-project-a6f12.firebaseapp.com',
@@ -19,3 +23,35 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
 export const analytics = getAnalytics(app)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+
+// Hook for logIn
+export const loginWithEmailAndPassword = async (
+  email: string,
+  password: string
+): Promise<UserCredential | any> => {
+  try {
+    return await signInWithEmailAndPassword(auth, email, password)
+  } catch (error: any) {
+    console.log(error.code, error.message)
+  }
+}
+
+// Hook for registration
+export interface IUser {
+  firstName: string
+  last: string
+  email: string
+  city: string
+}
+export const createLoginWithEmailAndPassword = async (
+  user: IUser,
+  password: string
+): Promise<UserCredential | any> => {
+  try {
+    return await createUserWithEmailAndPassword(auth, user.email, password)
+  } catch (error: any) {
+    console.log(error)
+  }
+}
