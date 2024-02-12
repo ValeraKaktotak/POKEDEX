1a5bc73f81479b3b57de8e29e2f5ed08ab38e616
Valera <valerakaktotak@gmail.com>
2024-02-12 22:49:03 +0200

    added SignUpForm component to Auth page

---
+++ src/pages/AuthPage/components/SignUpForm/SignUpForm.tsx
@@ -0,0 +1,81 @@
+import type { FC } from 'react'
+import type { SubmitHandler } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+import { Input } from '../../../../common/fields/inputs'
+import { userRegistration } from '../../../../firebase'
+import styles from '../../AuthPage.module.css'
+
+interface Inputs {
+  firstName: string
+  lastName: string
+  email: string
+  city: string
+  password: string
+}
+interface ISignUpForm {
+  isSignUp: boolean
+}
+
+export const SignUpForm: FC<ISignUpForm> = ({ isSignUp, ...props }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    userRegistration(data)
+  }
+
+  return (
+    <>
+      {isSignUp && (
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+          <Input
+            {...register('firstName', { required: true })}
+            placeholder='First Name'
+            disabled={isSubmitting}
+          />
+          <Input
+            {...register('lastName', { required: true })}
+            placeholder='Last Name'
+            disabled={isSubmitting}
+          />
+          <Input
+            {...register('email', { required: true })}
+            placeholder='Email'
+            disabled={isSubmitting}
+          />
+          <Input
+            {...register('city', { required: true })}
+            placeholder='City'
+            disabled={isSubmitting}
+          />
+          <Input
+            type='password'
+            {...register('password', { required: true })}
+            placeholder='Password'
+            disabled={isSubmitting}
+          />
+          {/* errors will return when field validation fails  */}
+          {errors.firstName && (
+            <span className={styles.error}>FirstName field is required</span>
+          )}
+          {errors.lastName && (
+            <span className={styles.error}>LastName field is required</span>
+          )}
+          {errors.email && (
+            <span className={styles.error}>Email field is required</span>
+          )}
+          {errors.password && (
+            <span className={styles.error}>Password field is required</span>
+          )}
+          <button type='submit' disabled={isSubmitting}>
+            Sign up
+          </button>
+        </form>
+      )}
+    </>
+  )
+}
