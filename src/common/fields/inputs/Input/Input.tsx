8a0adbf36e62db2bbdc4865549c7d30e508cf2e7
Valera <valerakaktotak@gmail.com>
2024-02-12 17:27:03 +0200

    have done with input component

--- src/common/fields/inputs/Input/Input.tsx
+++ src/common/fields/inputs/Input/Input.tsx
@@ -1,9 +1,17 @@
 import type { FC } from 'react'
+import styles from './Input.module.css'
 
 interface IInput extends React.ComponentPropsWithRef<'input'> {
   isLoading?: boolean
 }
 
-export const Input: FC<IInput> = () => {
-  return <div>Input</div>
+export const Input: FC<IInput> = ({ id, placeholder, ...props }) => {
+  console.log(id)
+
+  return (
+    <label className={styles.container} htmlFor={placeholder}>
+      <span className={styles.label}>{placeholder}</span>
+      <input className={styles.input} id={placeholder} {...props} />
+    </label>
+  )
 }
--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form'
 
 import { userRegistration } from '../../firebase'
 
+import { Input } from '../../common/fields/inputs'
 import styles from './AuthPage.module.css'
 
 interface Inputs {
@@ -31,24 +32,45 @@ export const AuthPage: FC = () => {
       {isSignUp && (
         // eslint-disable-next-line @typescript-eslint/no-misused-promises
         <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-          <label>First Name</label>
-          <input {...register('firstName', { required: true })} />
-          <label>Last Name</label>
-          <input {...register('lastName', { required: true })} />
-          <label>Email</label>
-          <input {...register('email', { required: true })} />
-          <label>City</label>
-          <input {...register('city', { required: true })} />
-          <label>Password</label>
-          <input
+          <Input
+            {...register('firstName', { required: true })}
+            placeholder='First Name'
+            disabled={isSubmitting}
+          />
+          <Input
+            {...register('lastName', { required: true })}
+            placeholder='Last Name'
+            disabled={isSubmitting}
+          />
+          <Input
+            {...register('email', { required: true })}
+            placeholder='Email'
+            disabled={isSubmitting}
+          />
+          <Input
+            {...register('city', { required: true })}
+            placeholder='City'
+            disabled={isSubmitting}
+          />
+          <Input
             type='password'
             {...register('password', { required: true })}
+            placeholder='Password'
+            disabled={isSubmitting}
           />
           {/* errors will return when field validation fails  */}
-          {errors.firstName && <span>FirstName field is required</span>}
-          {errors.lastName && <span>LastName field is required</span>}
-          {errors.email && <span>Email field is required</span>}
-          {errors.password && <span>Password field is required</span>}
+          {errors.firstName && (
+            <span className={styles.error}>FirstName field is required</span>
+          )}
+          {errors.lastName && (
+            <span className={styles.error}>LastName field is required</span>
+          )}
+          {errors.email && (
+            <span className={styles.error}>Email field is required</span>
+          )}
+          {errors.password && (
+            <span className={styles.error}>Password field is required</span>
+          )}
           <button type='submit' disabled={isSubmitting}>
             Sign up
           </button>
