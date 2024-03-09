6fb04406ac094be42c00384a4cd3861dcac62c0f
Valera <valerakaktotak@gmail.com>
2024-03-09 22:47:05 +0200

    added isLogin checking in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './routes/routesConfig'
 
 import './App.css'
+import { StoreContext } from './utils/context/store'
 
 const App: React.FC = () => {
+  const { store } = useContext(StoreContext)
+  if (!store.session.isLogin) return <h1>LOADING...</h1>
   return (
     <>
       <RouterProvider router={router} />
