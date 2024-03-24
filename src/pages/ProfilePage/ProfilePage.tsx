a818b035e153115b8b6eb4f7d877c4fa9ac83b44
Valera <valerakaktotak@gmail.com>
2024-03-24 22:09:46 +0200

    created hook to get user data

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,11 +1,20 @@
+import { collection, query, where } from 'firebase/firestore'
 import { useContext, type FC } from 'react'
 import { StoreContext } from '../../utils/context/store'
+import { db } from '../../utils/firebase'
+import { useCollection } from '../../utils/firebase/hooks/useCollection'
 import styles from './ProfilePage.module.css'
 
 export const ProfilePage: FC = () => {
   const { store } = useContext(StoreContext)
   console.log(store.userProfile)
   const user = store.userProfile
+
+  const q = query(
+    collection(db, 'pokemons'),
+    where('uid', '==', store.userProfile?.uid)
+  )
+  const { data } = useCollection(q)
 
   return (
     <>
---
+++ src/utils/firebase/hooks/useCollection.ts
@@ -0,0 +1,22 @@
+import { onSnapshot, type Query } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+
+export const useCollection = <T>(query: Query<T>): any => {
+  const [data, setData] = useState<any | null>(null)
+
+  useEffect(() => {
+    const unsub = onSnapshot(query, (querySnapshot) => {
+      const pokemons: any = []
+      querySnapshot.forEach((doc) => {
+        pokemons.push(doc.data())
+      })
+      console.log('Current data: ', pokemons)
+      setData(data)
+    })
+    return () => {
+      unsub()
+    }
+  }, [])
+
+  return { data }
+}
