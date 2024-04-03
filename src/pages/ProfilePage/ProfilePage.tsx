2980bc4aa8de69916ef068e9b241f8459558c47c
Valera <valerakaktotak@gmail.com>
2024-04-03 16:52:56 +0300

    added My Team in profile Page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,11 +1,19 @@
+import { type User } from 'firebase/auth'
 import { collection, query, where } from 'firebase/firestore'
 import { useContext, type FC } from 'react'
 import { Button } from '../../common/buttons/Button/Button'
+import { type IPokemon } from '../../utils/api/requests/pokemon/id/types'
 import { StoreContext } from '../../utils/context/store'
 import { db } from '../../utils/firebase'
 import { useCollection } from '../../utils/firebase/hooks/useCollection'
 import { useUserLogOutMutation } from '../../utils/firebase/hooks/useUserLogOutMutation'
 import styles from './ProfilePage.module.css'
+
+export interface ITeamMember {
+  pokemonID: IPokemon['id']
+  pokemonName: IPokemon['name']
+  uid: User['uid'] | undefined
+}
 
 export const ProfilePage: FC = () => {
   const { store } = useContext(StoreContext)
@@ -16,7 +24,9 @@ export const ProfilePage: FC = () => {
     collection(db, 'pokemons'),
     where('uid', '==', store.userProfile?.uid)
   )
-  const { data } = useCollection(q)
+
+  const data: ITeamMember[] | null = useCollection(q)
+  console.log(data)
 
   return (
     <>
@@ -25,6 +35,14 @@ export const ProfilePage: FC = () => {
         <img src={user?.photoURL ?? ''} alt='user photo' />
         <div>Hello {user?.displayName}</div>
         <Button onClick={mutate}>Log Out</Button>
+        <div>
+          <h2>My Team</h2>
+          {data
+            ? data.map((member) => (
+                <div key={member?.pokemonID}>{member?.pokemonName}</div>
+              ))
+            : ''}
+        </div>
       </div>
     </>
   )
--- src/utils/firebase/hooks/useCollection.ts
+++ src/utils/firebase/hooks/useCollection.ts
@@ -1,22 +1,27 @@
 import { onSnapshot, type Query } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 
+// export interface ITeamMember {
+//   pokemonID: IPokemon['id']
+//   pokemonName: IPokemon['name']
+//   uid: User['uid'] | undefined
+// }
+
 export const useCollection = <T>(query: Query<T>): any => {
-  const [data, setData] = useState<any | null>(null)
+  const [data, setData] = useState<T[] | null>(null)
 
   useEffect(() => {
     const unsub = onSnapshot(query, (querySnapshot) => {
-      const pokemons: any = []
+      const pokemons: T[] = []
       querySnapshot.forEach((doc) => {
         pokemons.push(doc.data())
       })
-      // console.log('Current data: ', pokemons)
-      setData(data)
+      setData(pokemons)
     })
     return () => {
       unsub()
     }
   }, [])
 
-  return { data }
+  return data
 }
